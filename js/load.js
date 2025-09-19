57bf09c607be1c5f8b22c6f9789144e730562d23
QiuTedYuan <yqiuac@gmail.com>
2025-09-19 09:03:38 +0800

    update nips paper

--- js/load.js
+++ js/load.js
@@ -37,52 +37,8 @@ $(document).ready(function () {
             document.getElementById("gallery").className = "active";
             break;
     }
-    // Load Publication
-	if (document.getElementById("pub-list")) {
-    document.getElementById("pub-list").innerHTML = `
-		<li>
-			<strong>Yuan Qiu</strong> and Ke Yi.&nbsp;
-			Approximate DBSCAN under Differential Privacy.&nbsp;
-			<i>ACM SIGMOD International Conference on Management of Data</i> (<strong>SIGMOD</strong>), June 2025.&nbsp;
-			[<a href="https://arxiv.org/abs/2508.08749" target="_blank">PDF</a>]
-			[<a href="file/sigmod25_poster.pdf" target="_blank">Poster</a>]
-		</li>    
-		<li>
-			Dajun Sun, Wei Dong, <strong>Yuan Qiu</strong> and Ke Yi.&nbsp;
-			Personalized Truncation for Personalized Privacy.&nbsp;
-			<i>ACM SIGMOD International Conference on Management of Data</i> (<strong>SIGMOD</strong>), June 2025.&nbsp;
-			[<a href="https://dl.acm.org/doi/10.1145/3698825" target="_blank">PDF</a>]
-			[<a href="file/sigmod25_poster_sun.pdf" target="_blank">Poster</a>]
-		</li>
-		<li>
-			Ziyue Huang, <strong>Yuan Qiu</strong>, Ke Yi and Graham Cormode.&nbsp;
-			Frequency Estimation Under Multiparty Differential Privacy: One-shot and Streaming. &nbsp;
-			<i>International Conference on Very Large Databases</i> (<strong>VLDB</strong>), September 2022. &nbsp;
-			[<a href="https://dl.acm.org/doi/10.14778/3547305.3547312" target="_blank">PDF</a>]
-			[<a href="file/vldb21_poster.pdf" target="_blank">Poster</a>]
-		</li>
-		<li>
-			<strong>Yuan Qiu</strong>, Wei Dong, Ke Yi, Bin Wu and Feifei Li. &nbsp;
-			Releasing Private Data for Numerical Queries. &nbsp;
-			<i>ACM SIGKDD Conference on Knowledge Discovery and Data Mining</i> (<strong>KDD</strong>), August 2022. &nbsp;
-			[<a href="https://dl.acm.org/doi/10.1145/3534678.3539424" target="_blank">PDF</a>]
-		</li>               
-		<li>
-			<strong>Yuan Qiu</strong>, Yilei Wang, Ke Yi, Feifei Li, Bin Wu and Chaoqun Zhan. &nbsp;
-			Weighted Distinct Sampling: Cardinality Estimation for SPJ Queries. &nbsp;
-			<i>ACM SIGMOD International Conference on Management of Data</i> (<strong>SIGMOD</strong>), June 2021. &nbsp;
-			[<a href="https://dl.acm.org/doi/10.1145/3448016.3452821" target="_blank">PDF</a>]
-			[<a href="http://home.cse.ust.hk/~yike/spj-full.pdf" target="_blank">Full Version</a>]
-		</li>
-		<li>
-			<strong>Yuan Qiu</strong>, Serafeim Papadias and Ke Yi. &nbsp;
-			Streaming HyperCube: A Massively Parallel Stream Join Algorithm. &nbsp;
-			<i>International Conference on Extending DataBase Technology</i> (<strong>EDBT</strong>), March 2019. Short paper. &nbsp;
-			[<a href="https://openproceedings.org/2019/conf/edbt/EDBT19_paper_224.pdf" target="_blank">PDF</a>]
-			[<a href="file/edbt19_poster.pdf" target="_blank">Poster</a>]
-		</li>
-	`;}
-    // Load footer
+   
+   // Load footer
     document.getElementById("contact-section").innerHTML = `
 		 <div class="col-xs-10 col-sm-10 col-md-10 col-lg-10 col-xs-offset-1 col-sm-offset-1 col-md-offset-1 col-lg-offset-1 text-center">
 			<img src="./img/contact.png" alt="contact icon" class="">
@@ -106,4 +62,98 @@ $(document).ready(function () {
 			<h5>3 Sept 2025</h5>
 		 </div>
      `;
+});
+
+window.addEventListener("DOMContentLoaded", function () {
+	if (document.getElementById("pub-list")) {
+	var publist = []
+	    publist.push({
+	    	conf: "NeurIPS",
+	    	authors: "<strong>Yuan Qiu</strong> and Ke Yi",
+	    	title: "Differential Privacy on Fully Dynamic Streams",
+	    	fullConf: "Conference on Neural Information Processing Systems",
+	    	fullDate: "December 2025",
+	    	remarks: '<strong style="color: #CC0000">Spotlight (3%)</strong>',
+	    	link: '[<a href="https://arxiv.org/abs/2209.01387" target="_blank">Full Version</a>]'
+	    },{
+	    	conf: "NeurIPS",
+	    	authors: "Dajun Sun, Wei Dong, <strong>Yuan Qiu</strong>, Ke Yi and Graham Cormode",
+	    	title: "Sum Estimation under Personalized Local Differential Privacy",
+	    	fullConf: "Conference on Neural Information Processing Systems",
+	    	fullDate: "December 2025",
+	    },{
+	    	conf: "SIGMOD",
+	    	authors: "<strong>Yuan Qiu</strong> and Ke Yi",
+	    	title: "Approximate DBSCAN under Differential Privacy",
+	    	fullConf: "ACM SIGMOD International Conference on Management of Data",
+	    	fullDate: "June 2025",
+	    	link: '[<a href="https://arxiv.org/abs/2508.08749" target="_blank">PDF</a>] [<a href="file/sigmod25_poster.pdf" target="_blank">Poster</a>]'
+	    },{
+	    	conf: "SIGMOD",
+	    	authors: "Dajun Sun, Wei Dong, <strong>Yuan Qiu</strong> and Ke Yi",
+	    	title: "Personalized Truncation for Personalized Privacy",
+	    	fullConf: "ACM SIGMOD International Conference on Management of Data",
+	    	fullDate: "June 2025",
+	    	link: '[<a href="https://dl.acm.org/doi/10.1145/3698825" target="_blank">PDF</a>] [<a href="file/sigmod25_poster_sun.pdf" target="_blank">Poster</a>]'
+	    },{
+	    	conf: "VLDB",
+	    	authors: "Ziyue Huang, <strong>Yuan Qiu</strong>, Ke Yi and Graham Cormode",
+	    	title: "Frequency Estimation Under Multiparty Differential Privacy: One-shot and Streaming",
+	    	fullConf: "International Conference on Very Large Databases",
+	    	fullDate: "September 2022",
+	    	link: '[<a href="https://dl.acm.org/doi/10.14778/3547305.3547312" target="_blank">PDF</a>] [<a href="file/vldb21_poster.pdf" target="_blank">Poster</a>]'
+	    },{
+	    	conf: "KDD",
+	    	authors: "<strong>Yuan Qiu</strong>, Wei Dong, Ke Yi, Bin Wu and Feifei Li",
+	    	title: "Releasing Private Data for Numerical Queries",
+	    	fullConf: "ACM SIGKDD Conference on Knowledge Discovery and Data Mining",
+	    	fullDate: "August 2022",
+	    	link: '[<a href="https://dl.acm.org/doi/10.1145/3534678.3539424" target="_blank">PDF</a>]'
+	    },{
+	    	conf: "SIGMOD",
+	    	authors: "<strong>Yuan Qiu</strong>, Yilei Wang, Ke Yi, Feifei Li, Bin Wu and Chaoqun Zhan",
+	    	title: "Weighted Distinct Sampling: Cardinality Estimation for SPJ Queries",
+	    	fullConf: "ACM SIGMOD International Conference on Management of Data",
+	    	fullDate: "June 2021",
+	    	link: '[<a href="https://dl.acm.org/doi/10.1145/3448016.3452821" target="_blank">PDF</a>] [<a href="http://home.cse.ust.hk/~yike/spj-full.pdf" target="_blank">Full Version</a>]'
+	    },{
+	    	conf: "EDBT",
+	    	authors: "<strong>Yuan Qiu</strong>, Serafeim Papadias and Ke Yi",
+	    	title: "Streaming HyperCube: A Massively Parallel Stream Join Algorithm",
+	    	fullConf: "International Conference on Extending DataBase Technology",
+	    	fullDate: "March 2019",
+	    	remarks: "Short Paper",
+	    	link: '[<a href="https://openproceedings.org/2019/conf/edbt/EDBT19_paper_224.pdf" target="_blank">PDF</a>] [<a href="file/edbt19_poster.pdf" target="_blank">Poster</a>]'
+	    });
+	    pubstr = []
+	    for (const pub of publist) {
+	    	tmp = []
+	    	tmp.push("<li><div class='pub-item'><span class='conf'>(",
+	    		pub.conf,
+	    		' \'',
+				pub.fullDate.slice(-2),
+	    		')</span>',
+	    		"<span class='details'>",
+	    		pub.authors,
+	    		". <u>",
+	    		pub.title,
+	    		"</u>.<br>\n",
+	    		"In <i>",
+	    		pub.fullConf,
+	    		"</i>, ",
+	    		pub.fullDate,
+	    		". ",
+	    	)
+	    	if (pub.remarks && pub.remarks.length) {
+	    		tmp.push(pub.remarks, ". ");
+	    	}
+	    	if (pub.link && pub.link.length) {
+	    		tmp.push(pub.link);
+	    	}
+	    	tmp.push("</span></div></li>")
+	    	pubstr.push(tmp.join(""))
+	    }
+	
+    	document.getElementById("pub-list").innerHTML = pubstr.join("\n");
+	}   
 });
